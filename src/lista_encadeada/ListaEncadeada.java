5e36fdbe4fd63ad277c1fdb930b1e79a4c3235a4
matheusm0ura <matheusdj49@live.com>
2020-12-06 17:49:44 -0300

    Lista encadeada.

--- src/lista_encadeada/ListaEncadeada.java
+++ src/lista_encadeada/ListaEncadeada.java
@@ -41,7 +41,15 @@ public class ListaEncadeada<T> implements TADListaEncadeada <T>{
         else if(posicao < 0 || posicao > tamanho){
             System.out.println("Posição inválida para inserção.");
         }
-
+        No<T> novoNo = new No<T>(valor, null);
+        No<T> noAnteriorInsercao = cabeca;
+        for(int i = 0; i < posicao - 1; i ++){
+            noAnteriorInsercao = noAnteriorInsercao.getProximo();
+        }
+        No<T> noPosteriorInserccao = noAnteriorInsercao.getProximo();
+        noAnteriorInsercao.setProximo(novoNo);
+        novoNo.setProximo(noPosteriorInserccao);
+        tamanho++;
     }
 
     @Override
@@ -59,17 +67,54 @@ public class ListaEncadeada<T> implements TADListaEncadeada <T>{
 
     @Override
     public T removePosicao(int posicao) {
-        return null;
+        if (posicao < 0 || posicao >= tamanho){
+            System.out.println("Não é possível remover. Posição inválida.");
+            return null;
+        }
+        No<T> noAnteriorRemocao = cabeca;
+
+        //Encontra o nó anterior que será removido
+        for (int i = 0; i < posicao - 1; i++){
+            noAnteriorRemocao = noAnteriorRemocao.getProximo();
+        }
+
+        No <T> noRemovido = noAnteriorRemocao.getProximo();
+
+        if(posicao == 0){
+            noRemovido = cabeca;
+            cabeca = noRemovido.getProximo();
+        }
+        else {
+            noAnteriorRemocao.setProximo(noRemovido.getProximo());
+            noRemovido.setProximo(null);
+
+        }
+        tamanho--;
+
+        return noRemovido.getValor();
     }
 
     @Override
     public boolean listaVazia() {
+        if (cabeca == null){
+            return true;
+        }
         return false;
+
     }
 
     @Override
     public void imprimiLista() {
-
+        if (listaVazia()){
+            System.out.println("Lista vazia. Não há elementos para impressão.");
+            return;
+        }
+        No<T> auxiliar = cabeca;
+        while (auxiliar != null){
+            System.out.print(" => " + auxiliar.getValor());
+            auxiliar = auxiliar.getProximo();
+        }
+        System.out.println();
     }
 
     public  No<T> getCauda(){
